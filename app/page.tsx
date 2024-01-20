104b457e52e0b4af89307b467c0c57c0d5834f18
sameer-gits <ms3054825@gmail.com>
2024-01-20 20:51:15 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,6 @@ import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const Card = () => {
-    const handleWheel = (event: React.WheelEvent) => {
-      // Increment or decrement the rotation based on the scroll direction
-      setRotation(
-        (prevRotation) =>
-          prevRotation + (event.deltaY < 0 ? rotationStep : -rotationStep)
-      );
-    };
-
-    useEffect(() => {
-      // Add event listener for wheel scroll
-      document.getElementById("anchor").addEventListener("wheel", handleWheel);
-
-      // Remove event listener on component unmount
-      return () => {
-        document
-          .getElementById("anchor")
-          .removeEventListener("wheel", handleWheel);
-      };
-    }, []);
-
     const images = [
       "https://images.pexels.com/photos/6875562/pexels-photo-6875562.jpeg?auto=compress&cs=tinysrgb&w=1000",
       "https://images.pexels.com/photos/19149193/pexels-photo-19149193/free-photo-of-cup-of-cocoa-drink-and-an-open-book.jpeg?auto=compress&cs=tinysrgb&w=1000",
@@ -44,31 +24,59 @@ export default function Home() {
     ];
     const [rotation, setRotation] = useState(0);
     const rotationStep = 360 / images.length; // Adjust this value to control the rotation speed
+    useEffect(() => {
+      const handleWheel = (event: React.WheelEvent) => {
+        // Increment or decrement the rotation based on the scroll direction
+        setRotation(
+          (prevRotation) =>
+            prevRotation + (event.deltaY < 0 ? rotationStep : -rotationStep)
+        );
+      };
+      document
+        .getElementById("anchor")
+        ?.addEventListener("wheel", handleWheel as any);
+
+      // Remove event listener on component unmount
+      return () => {
+        document
+          .getElementById("anchor")
+          ?.removeEventListener("wheel", handleWheel as any);
+      };
+    }, []);
+
     return (
       <main>
-        <div
-          id="anchor"
-          className="h-[700px] w-full relative overflow-hidden border-2 border-blue-400 "
-        >
-          {images.map((imageUrl, index) => (
-            <div
-              key={`item${index}`}
-              className="w-72 h-96 absolute rounded-3xl overflow-hidden top-[215%] left-1/2 border-2 transition-transform duration-300 ease-in-out"
-              style={{
-                transformOrigin: "top left",
-                transform: `rotate(${
-                  rotation + index * (360 / images.length)
-                }deg) translate( -50%, -350%)`,
-              }}
-            >
-              <img
-                src={imageUrl}
-                className="object-cover w-full h-full"
-                alt={`item${index + 1}`}
-              />
-            </div>
-          ))}
+        <div className="bg-slate-900 h-screen"></div>
+        <div id="track" className="bg-slate-700 relative h-[200vh]">
+          <div
+            id="anchor"
+            className=" min-h-screen w-full bg-slate-800 top-0 sticky overflow-hidden"
+          >
+            {images.map((imageUrl, index) => (
+              <div
+                id="card"
+                key={`item${index}`}
+                className="w-72 h-96 absolute  top-[1344px] left-1/2 transition-transform duration-300 ease-in-out"
+                style={{
+                  transformOrigin: "top left",
+                  transform: `rotate(${
+                    rotation + index * (360 / images.length)
+                  }deg) translate( -50%, -350%)`,
+                }}
+              >
+                <p className="absolute font-bold text-9xl  text-white bg-black p-4 rounded-3xl top-1/3 text-center -left-10">
+                  {index + 1}
+                </p>
+                <img
+                  src={imageUrl}
+                  className="object-cover rounded-3xl w-full h-full"
+                  alt={`item${index + 1}`}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+        <div className="bg-slate-900 h-screen"></div>
       </main>
     );
   };
